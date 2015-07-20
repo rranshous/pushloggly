a65bb3f1291e5c7cb80570a00e45e077adf78678
Robby Ranshous <rranshous@gmail.com>
2015-07-20 18:16:48 -0400

    consolidated the socket examples

--- socket_examples.rb
+++ socket_examples.rb
@@ -1,3 +1,6 @@
+### strait sockets
+# strait socket means you can use a select loop
+
 require 'pry'
 
 socket_path = "/var/run/docker.sock"
@@ -49,4 +52,24 @@ loop do
   #puts "UNPACKED: #{docker_stream_type} :: #{docker_chunk_size}"
 end
 
+
+
+## excon http library
+# easier to work with, def not as hacky
+
+require 'excon'
+container_id = ARGV.shift
+path = "/containers/#{container_id}/logs"
+socket_path = "/var/run/docker.sock"
+query_args = { stderr: 1, stdout: 1, timestamps: 1, follow: 1 }
+chunk_handler = lambda do |chunk, remaining_bytes, total_bytes|
+  if chunk.length == 8
+    #puts "#{chunk.unpack("CxxxN")}"
+  else
+    puts "#{chunk}"
+  end
+end
+Excon.get('unix://'+path, socket:socket_path, query:query_args,
+          response_block: chunk_handler)
+puts "DONE"
 
